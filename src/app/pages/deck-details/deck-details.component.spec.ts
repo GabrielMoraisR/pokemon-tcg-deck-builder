3310bc1f2d6d4da423953930787c6bfb9e1a29de
Gabriel de Morais <morais.g99@gmail.com>
2024-06-19 22:54:06 -0300

    chore: Add deck details component and its unit test

---
+++ src/app/pages/deck-details/deck-details.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeckDetailsComponent } from './deck-details.component';
+
+describe('DeckDetailsComponent', () => {
+  let component: DeckDetailsComponent;
+  let fixture: ComponentFixture<DeckDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DeckDetailsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DeckDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/deck-details/deck-details.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Deck } from '../../models/card.model';
+import { DeckService } from '../../services/deck.service';
+
+@Component({
+  selector: 'app-deck-details',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './deck-details.component.html',
+  styleUrls: ['./deck-details.component.css']
+})
+export class DeckDetailsComponent implements OnInit {
+  deck: Deck | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private deckService: DeckService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const deckIndex = params['id'];
+      this.deck = this.deckService.getDecks()[deckIndex];
+    });
+  }
+
+  getPokemonCount(): number {
+    return this.deck?.cards.filter(card => card.supertype === 'Pokémon').length || 0;
+  }
+
+  getTrainerCount(): number {
+    return this.deck?.cards.filter(card => card.supertype === 'Trainer').length || 0;
+  }
+
+  getUniqueTypes(): number {
+    const types = new Set(this.deck?.cards.flatMap(card => card.types));
+    return types.size;
+  }
+}
