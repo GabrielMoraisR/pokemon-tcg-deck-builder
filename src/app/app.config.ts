0d40d0088cb7c4a1f1d27a535442ec7f52ca82d8
Gabriel de Morais <morais.g99@gmail.com>
2024-06-20 19:12:53 -0300

    🚑 fix: Update app.config.ts and pokemon-tcg.service.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+export const appConfig = {
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes, withComponentInputBinding())
+  ]
 };
--- src/app/services/pokemon-tcg.service.ts
+++ src/app/services/pokemon-tcg.service.ts
@@ -1,17 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Card } from '../models/card.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonTcgService {
-  private apiUrl = 'https://api.pokemontcg.io/v2/cards';
+  private apiUrl = 'https://api.pokemontcg.io/v1/cards';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getCards(): Observable<{ data: Card[] }> {
-    return this.http.get<{ data: Card[] }>(this.apiUrl);
+  getCards(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
   }
 }
