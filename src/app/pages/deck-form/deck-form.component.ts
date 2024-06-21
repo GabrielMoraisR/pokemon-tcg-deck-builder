16e33088bc3c913e3c72b590a3f684c9925e4417
Gabriel de Morais <morais.g99@gmail.com>
2024-06-20 22:05:20 -0300

    ✨ feat: Update deck form component to create new decks

--- src/app/pages/deck-form/deck-form.component.ts
+++ src/app/pages/deck-form/deck-form.component.ts
@@ -1,66 +1,68 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, Router, RouterModule } from '@angular/router';
-import { Card, Deck } from '../../models/card.model';
+import { Router } from '@angular/router';
 import { DeckService } from '../../services/deck.service';
 import { PokemonTcgService } from '../../services/pokemon-tcg.service';
 
 @Component({
   selector: 'app-deck-form',
-  standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './deck-form.component.html',
-  styleUrls: ['./deck-form.component.css']
+  styleUrls: ['./deck-form.component.css'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
 export class DeckFormComponent implements OnInit {
-  deck: Deck = { name: '', cards: [] };
-  cards: Card[] = [];
-  isEditMode: boolean = false;
-  deckIndex: number = -1;
+  deckName: string = '';
+  selectedCard: string = '';
+  cards: any[] = [];
+  allCards: any[] = [];
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
     private pokemonTcgService: PokemonTcgService,
-    private deckService: DeckService
-  ) { }
+    private deckService: DeckService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-    this.pokemonTcgService.getCards().subscribe(response => {
-      this.cards = response.data;
-    });
-
-    this.route.params.subscribe(params => {
-      if (params['id']) {
-        this.isEditMode = true;
-        this.deckIndex = params['id'];
-        this.deck = this.deckService.getDecks()[this.deckIndex];
+  ngOnInit() {
+    this.pokemonTcgService.getCards().subscribe(
+      (data) => {
+        console.log('Cards data:', data);
+        this.allCards = data.data;
+      },
+      (error) => {
+        console.error('Erro ao buscar cartas:', error);
       }
-    });
+    );
   }
 
-  addCardToDeck(card: Card): void {
-    if (this.deck.cards.length < 60) {
-      const cardCount = this.deck.cards.filter(c => c.name === card.name).length;
-      if (cardCount < 4) {
-        this.deck.cards.push(card);
+  addCard() {
+    if (this.selectedCard) {
+      const card = this.allCards.find((card) => card.id === this.selectedCard);
+      if (card) {
+        this.cards.push(card);
+        this.selectedCard = '';
       }
     }
   }
 
-  removeCardFromDeck(index: number): void {
-    this.deck.cards.splice(index, 1);
-  }
-
-  saveDeck(): void {
-    if (this.deck.cards.length >= 24 && this.deck.cards.length <= 60) {
-      if (this.isEditMode) {
-        this.deckService.updateDeck(this.deckIndex, this.deck);
-      } else {
-        this.deckService.addDeck(this.deck);
-      }
+  createDeck() {
+    if (
+      this.deckName.trim() !== '' &&
+      this.cards.length >= 24 &&
+      this.cards.length <= 60
+    ) {
+      this.deckService.createDeck(this.deckName, this.cards);
+      this.clearForm();
       this.router.navigate(['/decks']);
+    } else {
+      alert('O baralho deve ter um nome e entre 24 e 60 cartas.');
     }
+  }
+
+  clearForm() {
+    this.deckName = '';
+    this.selectedCard = '';
+    this.cards = [];
   }
 }
