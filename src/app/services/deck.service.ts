893c059c9599c1d44c5e02d370e5e4d23ff37ca8
Gabriel de Morais <morais.g99@gmail.com>
2024-06-20 22:06:03 -0300

    ✨ feat: Refactor DeckService to implement DeckServiceInterface

--- src/app/services/deck.service.ts
+++ src/app/services/deck.service.ts
@@ -1,25 +1,57 @@
 import { Injectable } from '@angular/core';
-import { Deck } from '../models/card.model';
+
+export interface DeckServiceInterface {
+  createDeck(name: string, cards: any[]): void;
+  addCardToDeck(deck: any[], card: any): boolean;
+  removeCardFromDeck(deck: any[], card: any): void;
+  getDeckByName(name: string): any;
+  getDecks(): { id: number; name: string; cards: any[] }[];
+  deleteDeck(id: number): void;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class DeckService {
-  private decks: Deck[] = [];
+export class DeckService implements DeckServiceInterface {
+  private decks: { id: number; name: string; cards: any[] }[] = [];
 
-  getDecks(): Deck[] {
+  constructor() {}
+
+  createDeck(name: string, cards: any[]) {
+    const newDeck = {
+      id: this.decks.length + 1,
+      name,
+      cards,
+    };
+    this.decks.push(newDeck);
+  }
+
+  addCardToDeck(deck: any[], card: any): boolean {
+    const count = deck.filter((c) => c.name === card.name).length;
+    if (count < 4) {
+      deck.push(card);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  removeCardFromDeck(deck: any[], card: any) {
+    const index = deck.findIndex((c) => c.name === card.name);
+    if (index !== -1) {
+      deck.splice(index, 1);
+    }
+  }
+
+  getDeckByName(name: string): any {
+    return this.decks.find((deck) => deck.name === name);
+  }
+
+  getDecks(): { id: number; name: string; cards: any[] }[] {
     return this.decks;
   }
 
-  addDeck(deck: Deck): void {
-    this.decks.push(deck);
-  }
-
-  removeDeck(index: number): void {
-    this.decks.splice(index, 1);
-  }
-
-  updateDeck(index: number, deck: Deck): void {
-    this.decks[index] = deck;
+  deleteDeck(id: number): void {
+    this.decks = this.decks.filter((deck) => deck.id !== id);
   }
 }
